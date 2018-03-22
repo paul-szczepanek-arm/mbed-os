dcfab142da8590840cdd5f18a527cc1dc6d7ad6b
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2018-03-22 17:54:13 +0000

    don't request encrypt when pending

--- features/FEATURE_BLE/source/generic/GenericSecurityManager.cpp
+++ features/FEATURE_BLE/source/generic/GenericSecurityManager.cpp
@@ -873,7 +873,7 @@ void GenericSecurityManager::on_slave_security_request(
 
     if (pairing_required) {
         requestPairing(connection);
-    } else {
+    } else if (!cb->encryption_requested) {
         /* this will refresh keys if encryption is already present */
         enable_encryption(connection);
     }
