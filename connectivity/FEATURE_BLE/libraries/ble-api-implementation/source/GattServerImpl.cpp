8875a912e618d6e1147ab547fe5cbbd7f155866e
Paul Szczeanek <paul.szczepanek@arm.com>
2020-08-04 18:16:52 +0100

    move implementations to libraries directory

--- connectivity/FEATURE_BLE/libraries/ble-api-implementation/source/GattServerImpl.cpp
+++ connectivity/FEATURE_BLE/libraries/ble-api-implementation/source/GattServerImpl.cpp
@@ -16,8 +16,6 @@
  * limitations under the License.
  */
 
-#if (BLE_API_IMPLEMENTATION == 1)
-
 #include "BLERoles.h"
 #include <algorithm>
 #include "ble/GattServer.h"
@@ -1517,5 +1515,3 @@ void GattServer::handleDataSentEvent(unsigned count)
 }
 
 } // namespace ble
-
-#endif // (BLE_API_IMPLEMENTATION == 1)
